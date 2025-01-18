9dec969b93da5c222ee32548c4d923013d43c3c7
iren012 <airenetriperhatingiawa@gmail.com>
2025-01-18 12:42:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const isAdmin = true;  // Ganti dengan false untuk meniru pengguna biasa
 
 // Simulasi data pengumuman
 const pengumumanList = [
-    { id: 1, judul: 'Libur Semester', isi: 'Libur semester dimulai tanggal 25 Januari 2025.' },
-    { id: 2, judul: 'Pendaftaran Siswa Baru', isi: 'Pendaftaran siswa baru dibuka pada 1 Februari 2025.' }
+    { id: 1, judul: 'Perayaan Hari Pendidikan Nasional', isi: 'Dalam rangka memperingati Hari Pendidikan Nasional yang jatuh pada tanggal 2 Mei 2025, SDN Beji 01 akan mengadakan berbagai kegiatan menarik yang melibatkan siswa dan orang tua. Kami mengundang seluruh warga sekolah untuk berpartisipasi dalam acara yang akan diumumkan lebih lanjut.
+
+.' },
+
 ];
 
 // Fungsi untuk menambah pengumuman
